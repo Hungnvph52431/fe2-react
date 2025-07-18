7f2b6ecd7c78fcb271b8ed19f1badbf854274949
Nguyễn Văn Hùng <hungnvph52431@gmail.com>
2025-07-18 14:36:48 +0700

    <UserList, BrandList, OrderList>

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import ProductList from './components/ProductList';
 import CategoryList from './components/CategoryList';
+import UserList from './components/UserList';
+import BrandList from './components/BrandList';
+import OrderList from './components/OrderList';
 
 function App() {
   return (
     <div style={{padding : "20px"}}>
       <ProductList />
       <CategoryList />
+      <UserList />
+      <BrandList />
+      <OrderList />
     </div>
   );
 }
